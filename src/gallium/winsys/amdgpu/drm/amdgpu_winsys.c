5858a115ea7e4bb00bd5245dbb9c62871f35c5b1
Ronie Salgado <roniesalg@gmail.com>
2016-02-11 06:17:33 -0300

    I added SI family detection to the amdgpu gallium winsys.

--- src/gallium/winsys/amdgpu/drm/amdgpu_winsys.c
+++ src/gallium/winsys/amdgpu/drm/amdgpu_winsys.c
@@ -169,6 +169,8 @@ static boolean do_winsys_init(struct amdgpu_winsys *ws)
       ws->info.chip_class = VI;
    else if (ws->info.family >= CHIP_BONAIRE)
       ws->info.chip_class = CIK;
+   else if(ws->info.family >= CHIP_TAHITI)
+      ws->info.chip_class = SI;
    else {
       fprintf(stderr, "amdgpu: Unknown family.\n");
       goto fail;
@@ -185,6 +187,26 @@ static boolean do_winsys_init(struct amdgpu_winsys *ws)
 
    /* family and rev_id are for addrlib */
    switch (ws->info.family) {
+   case CHIP_TAHITI:
+      ws->family = FAMILY_SI;
+      ws->rev_id = SI_TAHITI_P_A0;
+      break;
+   case CHIP_PITCAIRN:
+      ws->family = FAMILY_SI;
+      ws->rev_id = SI_PITCAIRN_PM_A0;
+      break;
+   case CHIP_VERDE:
+      ws->family = FAMILY_SI;
+      ws->rev_id = SI_CAPEVERDE_M_A0;
+      break;
+   case CHIP_OLAND:
+      ws->family = FAMILY_SI;
+      ws->rev_id = SI_OLAND_M_A0;
+      break;
+   case CHIP_HAINAN:
+      ws->family = FAMILY_SI;
+      ws->rev_id = SI_HAINAN_V_A0;
+      break;
    case CHIP_BONAIRE:
       ws->family = FAMILY_CI;
       ws->rev_id = CI_BONAIRE_M_A0;
